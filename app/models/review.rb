bcd917c3d43e8f037735caf965e12505efd69b4f
Maxi Amodei <maxi_amodei@hotmail.com>
2021-02-17 12:54:50 -0300

    Reference added to reviews, controllers created, models validated

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, :rating, :restaurant, presence: true
+  validates :rating, inclusion: { in: (0..5) }
+  validates :rating, numericality: { message: "Must be a number" }
 end
