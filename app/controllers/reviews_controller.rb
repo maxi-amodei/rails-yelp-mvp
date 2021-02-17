1d42b96f4606138eb83d83886f7ecccde45bfecb
Maxi Amodei <maxi_amodei@hotmail.com>
2021-02-17 15:01:36 -0300

    Seeds, controllers and Views Finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+    # needed to instantiate the form_for
+  end
+
+  def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content, :restaurant_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating 5 fake restaurants...'
+
+  restaurant_one = Restaurant.create!(
+    name: "Burguer",
+    address: "Libertador",
+    phone_number:"0800 999 555",
+    category:"french"
+  )
+  restaurant_two = Restaurant.create!(
+    name: "Dashisushi",
+    address: "Palermo",
+    phone_number:"0800 555 666",
+    category:"japanese"
+  )
+  restaurant_three = Restaurant.create!(
+    name: "Pizza Hut",
+    address: "Almagro",
+    phone_number:"0800 888 555",
+    category:"italian"
+  )
+  restaurant_four = Restaurant.create!(
+    name: "The best",
+    address: "Libertador",
+    phone_number:"0800 000 555",
+    category:"belgian"
+  )
+  restaurant_five = Restaurant.create!(
+    name: "China Town",
+    address: "Martinez",
+    phone_number:"0800 777 555",
+    category:"chinese"
+  )
+  puts 'Finished!'
+  # ["chinese", "italian", "japanese", "french", "belgian"]
+  # <Restaurant id: nil, name: nil, address: nil, phone_number: nil, category: nil, created_at: nil, updated_at: nil>
+  
